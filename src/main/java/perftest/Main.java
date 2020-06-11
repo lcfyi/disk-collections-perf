bd0f6614d02d960068d48669267052c3e57c35c0
Leslie Cheng <leslie.cheng5@gmail.com>
2020-06-11 11:22:26 -0700

    Clean temporary files on start.

--- src/main/java/perftest/Main.java
+++ src/main/java/perftest/Main.java
@@ -1,19 +1,42 @@
 package perftest;
 
+import java.io.File;
+import java.io.IOException;
+
 import perftest.benchmarks.*;
 
 public final class Main {
-    public static void main(String[] args) {
+    private static String TMP_DIR = "tmp";
+    private static String CHRONICLE_DB = TMP_DIR + "/chronicle.dat";
+    private static String MAP_DB = TMP_DIR + "/mapdb.dat";
+    private static String SERIALIZED_MAP_DB = TMP_DIR + "/smapdb.dat";
+    private static String LEVEL_DB = TMP_DIR + "/leveldb.dat";
+
+    public static void cleanTemporaryDatabases(File f) throws IOException {
+        if (f.getName().contains(".gitkeep")) {
+            return;
+        }
+        if (f.isDirectory()) {
+            for (File c : f.listFiles()) {
+                cleanTemporaryDatabases(c);
+            }
+        } else {
+            f.delete();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        cleanTemporaryDatabases(new File(TMP_DIR));
         Benchmark mapBenchmark = new MapBenchmark();
         mapBenchmark.run();
         System.out.println(mapBenchmark);
-        Benchmark cmBenchmark = new ChronicleMapBenchmark("chronicle.dat");
+        Benchmark cmBenchmark = new ChronicleMapBenchmark(CHRONICLE_DB);
         cmBenchmark.run();
         System.out.println(cmBenchmark);
-        Benchmark mdbBenchmark = new MapDBBenchmark("mapdb.dat");
+        Benchmark mdbBenchmark = new MapDBBenchmark(MAP_DB);
         mdbBenchmark.run();
         System.out.println(mdbBenchmark);
-        Benchmark smdbBenchmark = new MapDBBenchmark("smapdb.dat");
+        Benchmark smdbBenchmark = new SerializedMapDBBenchmark(SERIALIZED_MAP_DB);
         smdbBenchmark.run();
         System.out.println(smdbBenchmark);
     }
